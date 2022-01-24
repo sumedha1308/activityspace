f0fc34c97217cf21801b6f31aa464f574b05708c
Sumedha Deshpande <sumedhasd1308@gmail.com>
2022-01-24 17:47:23 +0530

    Adding catch block in app.js

--- app.js
+++ app.js
@@ -1,48 +1,56 @@
-const express = require('express');
-const path = require('path');
-var genuuid = require('uuid').v4;
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const express = require("express");
+const path = require("path");
+var genuuid = require("uuid").v4;
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 const app = express();
 
-const api = require('./server/api');
-const db = require('./server/db');
+const api = require("./server/api");
+const db = require("./server/db");
 
 //Configure .env
-require('dotenv').config();
+require("dotenv").config();
 
 //Set port as process.env.PORT if it is present otherwise set it to 4000
 const port = process.env.PORT || 4000;
 
 //Initiate connection with database
 db.connect({
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-}).then(() => {
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+})
+  .then(() => {
     //Handle /api with the api middleware
-    app.use('/api', session({
+    app.use(
+      "/api",
+      session({
         genid() {
-            return genuuid() // use UUIDs for session IDs
+          return genuuid(); // use UUIDs for session IDs
         },
         store: new MongoStore({ client: db.getClient() }),
         secret: process.env.SESSION_SECRET,
         resave: false,
         saveUninitialized: true,
-    }), api);
+      }),
+      api
+    );
 
     //Handle non-api routes with static build folder
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, "build")));
 
     //Return index.html for routes not handled by build folder
-    app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    app.get("*", function (req, res) {
+      res.sendFile(path.join(__dirname, "build", "index.html"));
     });
 
     //Start listening on port
     app.listen(port, () => {
-        console.log(`Server listening at port: ${port}`);
+      console.log(`Server listening at port: ${port}`);
     });
-});
+  })
+  .catch((err) => {
+    console.log(err);
+  });
