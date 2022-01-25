3005c6b99fbc704b92dd7187e750f0ddd3153797
Sumedha Deshpande <sumedhasd1308@gmail.com>
2022-01-25 05:57:40 +0530

    Changing file extensions

--- server/db.js
+++ server/db.js
@@ -1,32 +1,36 @@
 const mongoose = require("mongoose");
 
 const connect = (dbConfig) => {
-    try {
-        const {host, username, password, database} = dbConfig;
+  try {
+    const { host, username, password, database } = dbConfig;
 
-        let absoluteHostName = host;
-        if (username) {
-            if (password !== undefined) {
-                absoluteHostName = `${username}:${password}@${absoluteHostName}`;
-            } else {
-                absoluteHostName = `${username}@${absoluteHostName}`;
-            }
-        }
-
-        const mongoUri = `mongodb+srv://${absoluteHostName}/${database}?retryWrites=true&w=majority`;
-
-        return mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
-    } catch (err) {
-        console.log(`Error connecting to MongoDB: ${err}`);
-        throw err;
+    let absoluteHostName = host;
+    if (username) {
+      if (password !== undefined) {
+        absoluteHostName = `${username}:${password}@${absoluteHostName}`;
+      } else {
+        absoluteHostName = `${username}@${absoluteHostName}`;
+      }
     }
+
+    const mongoUri = `mongodb+srv://${absoluteHostName}/${database}?retryWrites=true&w=majority`;
+
+    return mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (err) {
+    console.log(`Error connecting to MongoDB: ${err}`);
+    throw err;
+  }
 };
 
 const getClient = () => {
-    return mongoose.connection.getClient();
-}
+  return mongoose.connection.getClient();
+};
 
 module.exports = {
-    connect,
-    getClient
+  connect,
+  getClient,
 };
